d707267f683e5eabba2797faab92bd8d4acd8e36
remus <albyremus@gmail.com>
2018-08-05 17:22:58 +0200

    ciao

--- teb-planner_ws/build/teb_planner/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/remus/Scrivania/teb-planner_ws/src/teb_planner
-# Build directory: /home/remus/Scrivania/teb-planner_ws/build/teb_planner
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
