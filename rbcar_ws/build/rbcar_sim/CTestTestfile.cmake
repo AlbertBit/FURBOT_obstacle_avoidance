e944006d23169a39304cd53ed0bbbbed941ad4bd
remus <albyremus@gmail.com>
2018-08-06 12:45:32 +0200

    final commit

--- rbcar_ws/build/rbcar_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/remus/Scrivania/rbcar_ws/src/rbcar_sim
-# Build directory: /home/remus/Scrivania/rbcar_ws/build/rbcar_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
